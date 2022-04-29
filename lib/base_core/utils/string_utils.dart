5eefae8fd5fff066085f31a8aef00c549924841c
dingzhiwen <dingzw@ibaodashi.com>
2022-04-29 12:12:02 +0800

    fix

--- lib/base_core/utils/string_utils.dart
+++ lib/base_core/utils/string_utils.dart
@@ -5,3 +5,9 @@ import 'package:crypto/crypto.dart';
 String md5Hex(String input) {
   return md5.convert(utf8.encode(input)).toString();
 }
+
+extension StringExt on String {
+  int toInt() {
+    return isEmpty ? 0 : int.parse(this);
+  }
+}

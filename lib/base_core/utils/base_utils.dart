0345d4b4709e593d7b5e8191230e45d2325af45c
dingzhiwen <dingzw@ibaodashi.com>
2022-07-15 16:03:39 +0800

    fix

--- lib/base_core/utils/base_utils.dart
+++ lib/base_core/utils/base_utils.dart
@@ -8,5 +8,6 @@ export 'package:rego/base_core/utils/screen_utils.dart';
 export 'package:rego/base_core/utils/string_utils.dart';
 export 'package:rego/base_core/utils/format_utils.dart';
 export 'package:rego/base_core/utils/math_util.dart';
+export 'package:rego/base_core/utils/currency_utils.dart';
 
 class BaseUtils {}
